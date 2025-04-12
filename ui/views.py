a1f5801d36f72773a69009872e089b8dd8337acd
bilal.moussaoui <bilal.moussaoui@estudiantat.upc.edu>
2025-04-12 23:53:18 +0200

    Julius Guard

--- ui/views.py
+++ ui/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    """
+    View for the home page.
+    """
+    
+    context = {
+        'documents': [
+            {'id': 1, 'title': 'Passport', 'icon': 'fa-file-image'},
+            {'id': 2, 'title': 'Client Profile', 'icon': 'fa-file-word'},
+            {'id': 3, 'title': 'Account Opening Form', 'icon': 'fa-file-pdf'},
+            {'id': 4, 'title': 'Client Description', 'icon': 'fa-file-text'},
+        ],
+        'document_status': None
+    }
+    return render(request, 'home.html', context)
+
+def document_view(request, doc_id):
+    """
+    View for displaying a specific document.
+    """
+    # Here you would get the document data from wherever it's stored
+    doc_data = {
+        'title': f'Document {doc_id}',
+        'content': f'Content of document {doc_id}',
+        'date': '2023-01-01',
+        'author': 'Author',
+    }
+    
+    context = {
+        'document_id': doc_id,
+        'document_data': doc_data,
+    }
+    
+    return render(request, f'viewer/document_{doc_id}.html', context)
